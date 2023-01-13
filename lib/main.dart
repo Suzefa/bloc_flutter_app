5ce167a0f3ae41efb524830d328bfe074be4ab8c
Ameer-Hamza <h.qureshi333@hotmail.com>
2023-01-13 18:14:57 +0500

    provide bloc to material app for access globally

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,27 @@
 import 'package:example_project/presentation/route_management/route_generator.dart';
 import 'package:example_project/presentation/utilities/color_constant.dart';
+import 'package:example_project/presentation/utilities/multi_bloc_providers_list.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 void main() {
   runApp(const MyApp());
 }
+
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      onGenerateRoute: RouteGenerator.generateRoute,
-      theme: ThemeData(
-        primaryColor: ColorConstant.kPrimaryColor,
+    return MultiBlocProvider(
+      providers: MultiBlocProviderList.providers(),
+      child: MaterialApp(
+        onGenerateRoute: RouteGenerator.generateRoute,
+        theme: ThemeData(
+          primaryColor: ColorConstant.kPrimaryColor,
+        ),
+        debugShowCheckedModeBanner: false,
       ),
-      debugShowCheckedModeBanner: false,
     );
   }
 }
