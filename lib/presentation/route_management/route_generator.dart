2d57c046551ee288b6d2a2bc85d711af425094e2
Ameer-Hamza <h.qureshi333@hotmail.com>
2023-01-13 18:14:33 +0500

    Add new route for item view screen

--- lib/presentation/route_management/route_generator.dart
+++ lib/presentation/route_management/route_generator.dart
@@ -1,4 +1,6 @@
+import 'package:example_project/data/models/explore_item_model.dart';
 import 'package:example_project/presentation/custom_screens/home_screen.dart';
+import 'package:example_project/presentation/custom_screens/item_view_screen.dart';
 import 'package:example_project/presentation/route_management/route_names.dart';
 import 'package:flutter/material.dart';
 
@@ -7,7 +9,14 @@ class RouteGenerator {
   static Route<dynamic>? generateRoute(RouteSettings settings){
     switch (settings.name){
       case RouteNames.kInitialRoute:
-        return MaterialPageRoute(builder: (context)=> const HomeScreen());
+        return MaterialPageRoute(builder: (context)=> const HomeScreen(),);
+      case RouteNames.kItemViewScreenRoute:
+        ExploreItemModel argument = settings.arguments as ExploreItemModel;
+        return MaterialPageRoute(
+          builder: (context)=> ItemViewScreen(
+            exploreItemModel: argument,
+          ),
+        );
       default :
         return null;
     }
