2bc886fec6ae9eeeb2e16386bf5b85a0b10b08bf
Ameer-Hamza <h.qureshi333@hotmail.com>
2023-01-23 17:52:18 +0500

    add custom text filed enable property to handle during service hit also add disable field border decoration

--- lib/presentation/custom_widgets/custom_text_field.dart
+++ lib/presentation/custom_widgets/custom_text_field.dart
@@ -11,10 +11,11 @@ class CustomTextField extends StatelessWidget {
   final Color? borderColor;
   final Widget? suffixIcon;
   final bool isObscure;
+  final bool isEnabled;
   final TextInputAction? inputAction;
   final TextInputType? inputType;
   final ValueChanged<String>? onInputText;
-  const CustomTextField({Key? key,this.isObscure=false, this.suffixIcon , required this.textEditingController, required this.focusNode, this.prefixIcon,this.elevation, this.borderRadius, this.hintText, this.inputAction, this.inputType, this.onInputText, this.borderColor}) : super(key: key);
+  const CustomTextField({Key? key,this.isObscure=false, this.suffixIcon , required this.textEditingController, required this.focusNode, this.prefixIcon,this.elevation, this.borderRadius, this.hintText, this.inputAction, this.inputType, this.onInputText, this.borderColor, this.isEnabled = true}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -22,6 +23,7 @@ class CustomTextField extends StatelessWidget {
       borderRadius: BorderRadius.circular(borderRadius ?? 10.0,),
       elevation: elevation ?? 10.0,
       child: TextField(
+        enabled: isEnabled,
         obscureText: isObscure,
         onChanged: onInputText,
         controller: textEditingController,
@@ -51,9 +53,22 @@ class CustomTextField extends StatelessWidget {
                   : ColorConstant.kBlackColor.withOpacity(0.6),
             ),
           ),
+          disabledBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(borderRadius ?? 10.0,),
+            borderSide: BorderSide(
+              color: borderColor!=null
+                  ? borderColor!
+                  : ColorConstant.kBlackColor.withOpacity(0.6),
+            ),
+          ),
           focusedBorder: OutlineInputBorder(
             borderRadius: BorderRadius.circular(borderRadius ?? 10.0,),
-            borderSide: const BorderSide(color: ColorConstant.kBlackColor,width: 1.5,),
+            borderSide: BorderSide(
+              color: borderColor!=null
+                  ? borderColor!
+                  : ColorConstant.kBlackColor,
+              width: 1.5,
+            ),
           ),
           border: OutlineInputBorder(
             borderRadius: BorderRadius.circular(borderRadius ?? 10.0,),
