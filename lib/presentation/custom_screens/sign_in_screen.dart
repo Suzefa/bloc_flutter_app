594fbe26f498a702d991298141f077a0c38468e2
Ameer-Hamza <h.qureshi333@hotmail.com>
2023-01-23 17:55:20 +0500

    implement sign in screen validation using bloc

--- lib/presentation/custom_screens/sign_in_screen.dart
+++ lib/presentation/custom_screens/sign_in_screen.dart
@@ -1,4 +1,5 @@
 import 'package:example_project/business_logics/bloc/sign_in/sign_in_screen_bloc.dart';
+import 'package:example_project/presentation/custom_widgets/custom_app_button.dart';
 import 'package:example_project/presentation/custom_widgets/custom_text_field.dart';
 import 'package:example_project/presentation/custom_widgets/custom_text_widget.dart';
 import 'package:example_project/presentation/utilities/color_constant.dart';
@@ -68,27 +69,43 @@ class SignInScreen extends StatelessWidget {
                         ),
                       ),
                     ),
-                    CustomTextField(
-                      hintText: "Email",
-                      prefixIcon: Icons.email,
-                      inputType: TextInputType.emailAddress,
-                      inputAction: TextInputAction.next,
-                      elevation: 0.0,
-                      textEditingController: signInScreenBloc.emailEditingController,
-                      focusNode: signInScreenBloc.emailFocusNode,
+                    BlocBuilder<SignInScreenBloc,SignInScreenState>(
+                      builder: (context, state){
+                        return CustomTextField(
+                          isEnabled: !state.isLoading,
+                          borderColor: state.emailFieldColor,
+                          onInputText: (value){
+                            signInScreenBloc.add(EmailValidationEvent());
+                          },
+                          hintText: "Email",
+                          prefixIcon: Icons.email,
+                          inputType: TextInputType.emailAddress,
+                          inputAction: TextInputAction.next,
+                          elevation: 0.0,
+                          textEditingController: signInScreenBloc.emailEditingController,
+                          focusNode: signInScreenBloc.emailFocusNode,
+                        );
+                      },
+                    ),
+                    BlocBuilder<SignInScreenBloc,SignInScreenState>(
+                      builder: (context, state){
+                        return Visibility(
+                          visible: state.emailErrorMessage.isNotEmpty,
+                          child: Padding(
+                            padding: const EdgeInsets.only(top: 10.0),
+                            child: Align(
+                              alignment: Alignment.centerRight,
+                              child: CustomTextWidget(
+                                text: state.emailErrorMessage,
+                                textColor: ColorConstant.kRedColor,
+                              ),
+                            ),
+                          ),
+                        );
+                      },
                     ),
                     const Padding(
                       padding: EdgeInsets.symmetric(vertical: 10.0),
-                      child: Align(
-                        alignment: Alignment.centerRight,
-                        child: CustomTextWidget(
-                          text: "Email:",
-                          textColor: ColorConstant.kRedColor,
-                        ),
-                      ),
-                    ),
-                    const Padding(
-                      padding: EdgeInsets.only(bottom: 10.0),
                       child: Align(
                         alignment: Alignment.centerLeft,
                         child: CustomTextWidget(
@@ -99,24 +116,25 @@ class SignInScreen extends StatelessWidget {
                     ),
                     BlocBuilder<SignInScreenBloc,SignInScreenState>(
                       builder: (context, state) {
-                        bool obscure=false;
-                        if(state is PasswordFieldObscure){
-                          obscure = state.showPassword;
-                        }else if(state is SignInScreenInitial){
-                          obscure = state.showPassword;
-                        }
                         return CustomTextField(
-                          isObscure: obscure,
+                          isEnabled: !state.isLoading,
+                          borderColor: state.passwordFieldColor,
+                          onInputText: (text){
+                            signInScreenBloc.add(PasswordValidationEvent());
+                          },
+                          isObscure: state.isPasswordFieldObscure,
                           hintText: "Password",
                           prefixIcon: Icons.lock,
                           suffixIcon: Builder(
                             builder: (context) {
                               return GestureDetector(
                                 onTap: (){
-                                  BlocProvider.of<SignInScreenBloc>(context).add(PasswordVisibleEvent());
+                                  signInScreenBloc.add(PasswordVisibleEvent());
                                 },
                                 child: Icon(
-                                  obscure ? Icons.visibility_off : Icons.visibility,
+                                  state.isPasswordFieldObscure
+                                      ? Icons.visibility_off
+                                      : Icons.visibility,
                                   color: ColorConstant.kBlackColor,
                                 ),
                               );
@@ -130,15 +148,92 @@ class SignInScreen extends StatelessWidget {
                         );
                       },
                     ),
-                    const Padding(
-                      padding: EdgeInsets.symmetric(vertical: 10.0),
+                    BlocBuilder<SignInScreenBloc,SignInScreenState>(
+                      builder: (context, state){
+                        return Visibility(
+                          visible: state.passwordErrorMessage.isNotEmpty,
+                          child: Padding(
+                            padding: const EdgeInsets.symmetric(vertical: 10.0),
+                            child: Align(
+                              alignment: Alignment.centerRight,
+                              child: CustomTextWidget(
+                                text: state.passwordErrorMessage,
+                                textColor: ColorConstant.kRedColor,
+                              ),
+                            ),
+                          ),
+                        );
+                      },
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(vertical: 10.0),
                       child: Align(
                         alignment: Alignment.centerRight,
-                        child: CustomTextWidget(
-                          text: "Email:",
-                          textColor: ColorConstant.kRedColor,
+                        child: Row(
+                          mainAxisSize: MainAxisSize.min,
+                          children: [
+                            const CustomTextWidget(
+                              text: "remember me",
+                              fontSize: 14.0,
+                            ),
+                            BlocBuilder<SignInScreenBloc, SignInScreenState>(
+                              builder: (context, state){
+                                return Padding(
+                                  padding: const EdgeInsets.only(left: 5.0,),
+                                  child: SizedBox(
+                                    height: 25.0,
+                                    width: 25.0,
+                                    child: Checkbox(
+                                      activeColor: ColorConstant.kBlackColor,
+                                      value: state.isRememberMeEnable,
+                                      onChanged: (v) {
+                                        if(state.isLoading){
+                                          return;
+                                        }
+                                        signInScreenBloc.add(RememberMeEvent());
+                                      },
+                                    ),
+                                  ),
+                                );
+                              },
+                            ),
+                          ],
                         ),
                       ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(top: 20.0,bottom: 10.0,),
+                      child: BlocBuilder<SignInScreenBloc,SignInScreenState>(
+                        builder: (context,state){
+                          return CustomAppButton(
+                            isLoading: state.isLoading,
+                            buttonText: "Sign In",
+                            onButtonPressed: ()=>
+                                signInScreenBloc.add(SignInButtonEvent()),
+                          );
+                        },
+                      )
+                    ),
+                    Row(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        const CustomTextWidget(
+                          text: "Don't have an account?",
+                          fontSize: 12.0,
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.only(left: 10.0,),
+                          child: GestureDetector(
+                            onTap: (){},
+                            child: const CustomTextWidget(
+                              text: "Sign Up",
+                              fontSize: 12.0,
+                              textColor: ColorConstant.kPrimaryColor,
+                              textDecoration: TextDecoration.underline,
+                            ),
+                          ),
+                        ),
+                      ],
                     ),
                   ],
                 ),
