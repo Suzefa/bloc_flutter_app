68cdedce4eb783538b545948ad075a00bd31bf23
Ameer-Hamza <h.qureshi333@hotmail.com>
2023-02-03 01:26:25 -0600

    initialize firebase into app

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,23 @@
 import 'dart:io';
 
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:google_fonts/google_fonts.dart';
 
+import 'firebase_options.dart';
 import 'presentation/route_management/route_generator.dart';
 import 'presentation/utilities/bloc_cubit_observer.dart';
 import 'presentation/utilities/color_constant.dart';
 import 'presentation/utilities/multi_bloc_providers_list.dart';
 
-void main() {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   Bloc.observer = BlocCubitObserver();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
     systemNavigationBarColor: Platform.isAndroid
         ? ColorConstant.kWhiteColor
